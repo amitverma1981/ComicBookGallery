b3a6c544df7046343f41210913077d0f3f8c2bd4
v.amit <v.amit@YI1013155LT>
2021-10-07 20:21:58 +0530

    Add project files.

---
+++ ComicBookGallery/Controllers/SarnController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class SarnController : Controller
+    {
+        public string Test()
+        {
+            return "Test!";
+        } 
+    }
+}
---
+++ ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
